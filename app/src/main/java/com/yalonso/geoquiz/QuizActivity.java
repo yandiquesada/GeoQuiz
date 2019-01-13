30f8112666f84aad0c956fef75abf6bd3938407c
Yandi Alonso <yandiquesada@gmail.com>
2019-01-13 16:03:50 -0500

    GEOQUIZ- Adding buttons, events, and id resources.

--- app/src/main/java/com/yalonso/geoquiz/QuizActivity.java
+++ app/src/main/java/com/yalonso/geoquiz/QuizActivity.java
@@ -2,12 +2,35 @@ package com.yalonso.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add code here!
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add code here!
+            }
+        });
     }
 }
