2a8b2d42df617ffc30693b8a3cfe3df60cfdcac5
Yandi Alonso <yandiquesada@gmail.com>
2019-01-15 20:56:49 -0500

    GEOQUIZ- Was implemented the class Question.

---
+++ app/src/main/java/com/yalonso/geoquiz/Question.java
@@ -0,0 +1,11 @@
+package com.yalonso.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
