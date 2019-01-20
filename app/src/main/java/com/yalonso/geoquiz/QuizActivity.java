62139c4458d86fcc97c147d385c2ea0c8a3fd811
Yandi Alonso <yandiquesada@gmail.com>
2019-01-20 15:33:29 -0500

    GEOQUIZ-Adding logs in the activity state changes callbacks.

--- app/src/main/java/com/yalonso/geoquiz/QuizActivity.java
+++ app/src/main/java/com/yalonso/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.yalonso.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -16,6 +17,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mNextButton;
     private TextView mQuestionTextView;
 
+    private static final String TAG = "QuizActivity";
+
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia,true),
             new Question(R.string.question_ocean,true),
@@ -29,8 +32,9 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "onCreate called");
 
+        setContentView(R.layout.activity_quiz);
         mQuestionTextView = findViewById(R.id.question_text_view);
         updateQuestionTextView();
 
@@ -97,6 +101,36 @@ public class QuizActivity extends AppCompatActivity {
             messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
     }
 
 }
